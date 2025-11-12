72285c99ddddacf22848233907b8555978fd69eb
hydra07 <abcxyzkhong@gmail.com>
2025-11-12 14:44:20 +0700

    feat: update fish & starship

--- fish/config-linux.fish
+++ fish/config-linux.fish
@@ -1,5 +1,10 @@
 if type -q exa
-  alias ll "exa -l -g --icons"
-  alias lla "ll -a"
-  alias pacman='pacman --color=always'
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+    alias pacman='pacman --color=always'
+end
+
+function fish_greeting
+    echo Hello friend!
+    echo The time is (set_color yellow)(date +%T)(set_color normal) and this machine is called $hostname
 end
--- fish/config.fish
+++ fish/config.fish
@@ -1,42 +1,32 @@
 if status is-interactive
-  set fish_greeting ""
-  
-  set -gx TERM xterm-256color
+    set fish_greeting ""
 
-  # THEME
-  set -g theme_color_scheme terminal-dark
-  set -g fish_prompt_pwd_dir_length 2
-  set -g theme_display_user yes
-  set -g theme_hide_hostname no
-  set -g theme_hostname always
-  
-  set -gx EDITOR nvim
-  
-  # PATH
-  set -gx PATH bin $PATH
-  set -gx PATH ~/bin $PATH
-  set -gx PATH ~/.local/bin $PATH
-  set -gx PATH ~/.bun/bin $PATH
+    set -gx TERM xterm-256color
 
-  # NVM
-  function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-      status --is-command-substitution; and return
-      if test -f .nvmrc; and test -r .nvmrc
-          nvm use
-        else
-      end
-  end
+    # THEME
+    set -g theme_color_scheme terminal-dark
+    set -g fish_prompt_pwd_dir_length 2
+    set -g theme_display_user yes
+    set -g theme_hide_hostname no
+    set -g theme_hostname always
 
-  # CONFIG OS
-  switch (uname)
-    case Linux
-        source (dirname (status --current-filename))/config-linux.fish
-    case '*'
-        source (dirname (status --current-filename))/config-windows.fish
-  end
+    set -gx EDITOR nvim
 
-  # ACTIVATE
-  mise activate fish | source 
-  starship init fish | source
+    # PATH
+    set -gx PATH bin $PATH
+    set -gx PATH ~/bin $PATH
+    set -gx PATH ~/.local/bin $PATH
+    set -gx PATH ~/.bun/bin $PATH
+
+    # CONFIG OS
+    switch (uname)
+        case Linux
+            source (dirname (status --current-filename))/config-linux.fish
+        case '*'
+            source (dirname (status --current-filename))/config-windows.fish
+    end
+
+    # ACTIVATE
+    mise activate fish | source
+    starship init fish | source
 end
-
